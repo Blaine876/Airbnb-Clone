fa0cf20f48e648c8372159550695bd99cf53a965
Blaine Oakleym <blaine.oakley@gmailcom>
2020-08-27 17:33:13 -0400

    Added footer to complete the Home Page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <h1>Footer</h1>
+      <p> &copy; 2020 Airbnb Clone. No rights reserved</p>
+      <p>Privacy &#5867; Terms &#5867; Sitemap &#5867; Company Details </p>
     </div>
   );
 }
